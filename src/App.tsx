b2644e4c101de6cfa4170cbdb26729dada8db312
Pitra <justin.pitra@gmail.com>
2025-02-04 11:23:33 +0100

    :tada: Init front

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-5xl font-bold underline">Hello World !</h1>
+        <Button className="ml-5"> Click me !</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
