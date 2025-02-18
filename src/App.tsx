edf1021d4d639ba031df71f4b566f55315ecd14b
MDorizon <mdorizon@proton.me>
2025-02-18 16:01:40 +0100

    :beers: navbar ended

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthFormPage";
 import ClassroomPage from "./pages/ClassroomPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { Toaster } from "react-hot-toast";
 import RlyImportantComponent from "./components/RlyImportantComponent";
-import LogoutButton from "./components/LogoutButton";
+import Navbar from "./components/ui/Navbar";
 import HomePage from "./pages/HomePage";
 import ClassroomDetailsPage from "./pages/ClassroomDetailsPage";
-import ManageClassroomsButton from "./components/AdminButton";
 import ReservationPage from "./pages/ReservationPage";
 import ClassroomListPage from "./pages/ClassroomListPage";
 import MyReservationsPage from "./pages/MyReservationPage";
 
 const AppContent = () => {
-  const location = useLocation();
-  const token = localStorage.getItem("access_token");
-
   return (
     <>
+      <Navbar />
       <Toaster position="top-right" />
       <RlyImportantComponent />
-      {token && location.pathname !== "/auth" && (
-        <div className="p-6 flex justify-end gap-4">
-          <ManageClassroomsButton />
-          <LogoutButton />
-        </div>
-      )}
 
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
